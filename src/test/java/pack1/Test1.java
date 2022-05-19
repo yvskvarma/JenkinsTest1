fae58a7f0ab32d6afafd825edb3c6a3be66a5a5c
kotesh <koteswarrao.chiruvolu@cultuzz.com>
2022-05-19 15:45:23 +0530

    added sample script

--- src/test/java/pack1/Test1.java
+++ src/test/java/pack1/Test1.java
@@ -1,7 +1,21 @@
 package pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Test1 {
 	
-	
+	@Test
+	public void OpenBrowser() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\eclipse-workspace\\JenkinsTest1\\src\\test\\resources\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+		
+	}
 
 }
